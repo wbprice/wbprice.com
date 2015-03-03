0d90bdb69e4e20a6eb503c9f97bbcfea4ba8ca8f
Blaine Price <1wbprice@gmail.com>
2015-03-03 18:01:23 -0500

    Add gulp deploy deps. If screen is 768px+, set the height of background element to 1/3 the innerHeight of the window.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,11 @@
+'use strict';
 /* jshint devel:true */
 console.log('\'Allo \'Allo!');
+
+$(function() {
+
+  if ( $(window).width() >= 768 ) {
+    $('.background').height( $(window).innerHeight() / 3);
+  }
+
+});
